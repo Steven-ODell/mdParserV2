fa313425271127018cdfadffc0a5b0e892867746
Steven ODell <saoii96@gmail.com>
2026-03-16 00:31:07 -0700

    Split files

--- InlineParserV3.js
+++ InlineParserV3.js
@@ -1,61 +1,3 @@
-import { renderer } from "./RendererV3.js";
-import { inlineParser } from "./InlineParserV3.js";
-
-const inputBox = document.getElementById("testArea");
-const outputDiv = document.getElementById("testDiv");
-let root = []
-
-const startingItems = [
-    { pattern: /^\#\#\#\s/,   type: "h3" },
-    { pattern: /^\#\#\s/,    type: "h2" },
-    { pattern: /^\#\s/,     type: "h1" },
-    { pattern: /^\>\s/,     type: "blockquote" },
-    { pattern: /^\-\s/ ,    type: "ul" },
-    { pattern:/^\d+\.\s/, type: "ol"},
-    { pattern:/^\-\-\-/, type: "hr"},
-]
-
-inputBox.addEventListener('input', ()  => {
-    const rootReadyForRender = blockParser(inputBox.value)
-    console.log(rootReadyForRender)
-    outputDiv.innerHTML = renderer(rootReadyForRender)
-})
-
-const blockParser = (inputString) => {
-
-    let splitStringArray = inputString.split("\n");
-    root.length = 0
-    let currentObject = {}
-    let nestLevel = 0 
-
-    splitStringArray.forEach(block => {
-        const matched = startingItems.find(patterns => {
-            if (block.match(patterns.pattern)) {
-                currentObject = {
-                    type: patterns.type,
-                    value: block.replace(patterns.pattern, ""),
-                    nestLevel: 0,
-                    children: []
-                }
-                root.push(currentObject)
-                return true
-            }
-        })
-        if (!matched) {
-            currentObject = {
-            type: "p",
-            value: block,
-            nestLevel: 0,
-            children: []
-            } 
-            root.push(currentObject)
-        }
-    })
-    root  = inlineParser(root)
-    console.log(JSON.stringify(root, null, 2))
-    return root
-}
-
 const inlineItems = [
     { pattern: "***", type: "strongEm" },
     { pattern: "**",  type: "strong" },
@@ -136,3 +78,4 @@ const inlineParser = (inputRoot) => {
     return inputRoot
 }
 
+export { inlineParser}
---
+++ RendererV3.js
@@ -0,0 +1,55 @@
+
+const rendererDict = [
+    { type:"hr", open: "<hr>", close: ""},
+    { type:"p", open: "<p>", close: "</p>"},
+    { type:"inlineText", open: "", close: ""},
+    { type:"h1", open: "<h1>", close: "</h1>"},
+    { type:"h2", open: "<h2>", close: "</h2>"},
+    { type:"h3", open: "<h3>", close: "</h3>"},
+    { type:"blockquote", open: "<blockquote>", close: "</blockquote>"},
+    { type:"ul", open: "<ul>", close: "</ul>"},
+    { type:"ol", open: "<ol>", close: "</ol>"},
+    { type:"code", open: "<code>", close: "</code>"},
+    { type:"mark", open: "<mark>", close: "</mark>"},
+    { type:"sup", open: "<sup>", close: "</sup>"},
+    { type:"sub", open: "<sub>", close: "</sub>"},
+    { type: "del", open: "<del>", close: "</del>"},
+    { type:"em", open: "<em>", close: "</em>"},
+    { type:"strong", open: "<strong>", close: "</strong>"},
+    { type:"strongEm", open: "<strong><em>", close: "</em></strong>"},
+
+]
+
+
+const renderer = (inputRoot) => {
+    
+    let finalString = ""
+
+    inputRoot.forEach(node => {
+        let blockLabel = rendererDict.find(d => {
+            if (d.type === node.type) { return true }
+        })
+        finalString += blockLabel.open
+
+        let blockChildren = node.children
+        if (blockChildren.length > 0) {
+            blockChildren.forEach(child => {
+                rendererDict.find(d => {
+                    let dictType = d.type
+                        if (child.type === dictType){
+                            finalString += d.open + child.value + d.close
+                            return true
+                        }
+                //console.log("ParentType:" + node.type)
+                //console.log("ChildType:" + child.type)
+                //console.log("Value:" + child.value)                  
+                })
+            })
+        }
+        finalString += blockLabel.close
+        console.log(finalString)
+    })
+    return finalString
+}
+
+export { renderer }
